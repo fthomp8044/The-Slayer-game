df9848a3363866c3bb024749b9881bbd90f8c73c
fthomp8044 <fthomp2430@gmail.com>
2020-07-08 21:56:26 -0400

    added more functions with a little bit of styling and refactoring

--- vue.js
+++ vue.js
@@ -4,17 +4,24 @@ new Vue ({
     playerHealth: 100,
     monsterHealth: 100,
     gameIsRunning: false,
+    turns: [],
   },
   methods: {
     startGame: function() {
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function() {
       //random number from 3-10
       // take the monsters health and reduce by the calculateDamage function
-      this.monsterHealth -= this.calculateDamage(3, 10);
+      let damage = this.calculateDamage(3,10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster for ' + damage
+      });
       if (this.checkWin()) {
         return;
       };
@@ -23,13 +30,44 @@ new Vue ({
       this.checkWin();
     },
     specialAttack: function() {
-
+      let damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster Hard for ' + damage
+      });
+      if (this.checkWin()) {
+        return;
+      }
+      this.monsterAttacks();
     },
+    //this prevents my health bar from going past 100.
     heal: function() {
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth = 100;
+      }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals 10'
+      });
+      this.monsterAttacks();
 
     },
     giveUp: function() {
+      this.gameIsRunning = false;
+      alert('You suck Bro');
 
+    },
+    monsterAttacks: function () {
+      let damage = this.calculateDamage(8, 14);
+      this.playerHealth -= damage;
+      this.checkWin();
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster Hist player for ' + damage
+      });
     },
     calculateDamage: function(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min)
